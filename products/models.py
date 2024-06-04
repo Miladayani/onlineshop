e92461ae7dc0a6f6a930780701e44784d58dbeee
Your Name <you@example.com>
2024-06-04 11:48:08 +0330

    add transalation to project

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
@@ -17,4 +19,37 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args={self.pk})
 
-# class Comment(models.Model):
+
+class ActiveCommentManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentManager, self).get_queryset().filter(active=True)
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', _('Very Bad')),
+        ('2', _('Bad')),
+        ('3', _('Normal')),
+        ('4', _('Good')),
+        ('5', _('Perfect')),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Comment author'
+    )
+    body = models.TextField(verbose_name=_('Comment text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_("What's your rate?"))
+
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    active_comments_manager = ActiveCommentManager()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
