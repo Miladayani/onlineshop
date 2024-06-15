95ce85ee45f3f65b50dc9a367f13e63d92bf7d14
Your Name <you@example.com>
2024-06-15 10:29:29 +0330

    add short description to product model

--- products/models.py
+++ products/models.py
@@ -7,10 +7,11 @@ from ckeditor.fields import RichTextField
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = RichTextField()
-    price = models.PositiveIntegerField(default=0)
-    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    short_description = models.TextField(blank=True, verbose_name=_('Short Description'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('Price'))
+    active = models.BooleanField(default=True, verbose_name=_('Active'))
     image = models.ImageField(verbose_name=_('Product Image'), upload_to="product/product_cover", blank=True)
 
     date_created = models.DateField(default=timezone.now, verbose_name=_('Date Created'))
@@ -37,19 +38,19 @@ class Comment(models.Model):
         ('5', _('Perfect')),
     ]
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name=_('Product') )
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name='Comment author'
+        verbose_name=_('Comment author')
     )
     body = models.TextField(verbose_name=_('Comment text'))
     stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_("What's your rate?"))
 
-    date_created = models.DateField(auto_now_add=True)
+    date_created = models.DateField(auto_now_add=True, verbose_name=_('Date Created'))
     date_modified = models.DateField(auto_now=True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True, verbose_name=_('Active'))
 
     objects = models.Manager()
     active_comments_manager = ActiveCommentManager()
